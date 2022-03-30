0b1c6620cad2b4e62257fd1c6bc48e6b14ad529d
siddharth733 <siddharthschauhan694@gmail.com>
2022-03-30 11:59:53 +0530

    second commit

--- index - Copy (8).php
+++ index - Copy (8).php
@@ -51,7 +51,7 @@
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="#">Abouts</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
